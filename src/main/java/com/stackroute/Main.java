49fa065290638deeb9677a16a2b179bb47ea9e44
sureshkumar007 <sureshkumar12074@gmail.com>
2019-07-29 08:49:23 +0530

    3rd branch done

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -18,8 +18,9 @@ public class Main
     public static void main( String[] args )
     {
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Movie mov1=context.getBean("movie1",Movie.class);
+        //Movie mov1=context.getBean("movie1",Movie.class);
         Movie mov2=context.getBean("movie2",Movie.class);
+
 //        System.out.println("checking test");
 //        Actor act=context.getBean("actor",Actor.class);
 //        System.out.println(act);
@@ -32,12 +33,12 @@ public class Main
 
 
 //        //Bean Factory
-        XmlBeanFactory factory = new XmlBeanFactory (new ClassPathResource("beans.xml"));
-        Movie mov3=context.getBean("movie2",Movie.class);
-        Movie mov4=context.getBean("movie2",Movie.class);
-        System.out.println(mov1==mov3);
-        System.out.println(mov1.getActor());
-        System.out.println(mov3.getActor());
+//        XmlBeanFactory factory = new XmlBeanFactory (new ClassPathResource("beans.xml"));
+//        Movie mov3=factory.getBean("movie2",Movie.class);
+//        Movie mov4=factory.getBean("movie2",Movie.class);
+//
+       // System.out.println(mov1.getActor());
+        System.out.println(mov2.getActor());
 
 //        System.out.println("testing bean factory");
 //        Actor act1=context.getBean("actor",Actor.class);
@@ -49,3 +50,14 @@ public class Main
 
     }
 }
+
+
+/*
+Exception in thread "main" org.springframework.beans.factory.BeanCreationException:
+ Error creating bean with name 'movie2' defined in class path resource [BeanFile.xml]:
+  Instantiation of bean failed; nested exception is org.springframework.beans.
+  BeanInstantiationException: Failed to instantiate [com.stackroute.domain.Movie]:
+  No default constructor found; nested exception is java.lang.NoSuchMethodException:
+  com.stackroute.domain.Movie.<init>()
+
+ */
