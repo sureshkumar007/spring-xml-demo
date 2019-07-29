2815ed0114a62a1df98f6fa20394589863fb3c71
sureshkumar007 <sureshkumar12074@gmail.com>
2019-07-29 09:19:31 +0530

    4th task done

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,32 +1,71 @@
 package com.stackroute.domain;
 
-public class Movie
-{
-    private Actor actor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 
-//    public Movie() {
+//public class Movie
+//{
+//    private Actor actor;
 //
+////    public Movie() {
+////
+////    }
+//
+//
+//    public Movie(Actor actor)
+//    {
+//        this.actor = actor;
 //    }
+//
+//    public Actor getActor() {
+//        return actor;
+//    }
+//
+//    public void setActor(Actor actor) {
+//        this.actor = actor;
+//        System.out.println("called");
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Movie{" +
+//                "actor=" + actor +
+//                '}';
+//    }
+public class Movie implements ApplicationContextAware, BeanFactoryAware, BeanNameAware
+{
+    Actor actor;
+    private ApplicationContext applicationContext = null;
+    private BeanFactory beanFactory;
 
-
-    public Movie(Actor actor)
-    {
+    //use setter injection in task1
+    public void setActor(Actor actor) {
         this.actor = actor;
     }
 
     public Actor getActor() {
         return actor;
     }
+    public Movie() {
+        super();
+    }
 
-    public void setActor(Actor actor) {
+    //use constructor-injection in task2
+    public Movie(Actor actor) {
         this.actor = actor;
-        System.out.println("called");
+    }
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+    public void setBeanName(String s) {
+        System.out.println(s);
+    }
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext=applicationContext;
     }
 
-    @Override
-    public String toString() {
-        return "Movie{" +
-                "actor=" + actor +
-                '}';
-    }
 }
